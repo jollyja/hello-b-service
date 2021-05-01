5af14cf2878028509dccdcf253c9521184994bba
王培嘉 <jollyja@gmail.com>
2021-05-01 22:50:20 +0800

    nacos

--- src/main/java/cn/hello/b/controller/BController.java
+++ src/main/java/cn/hello/b/controller/BController.java
@@ -1,35 +1,40 @@
 package cn.hello.b.controller;
 
-import com.alibaba.nacos.api.annotation.NacosInjected;
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.api.naming.NamingService;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Log
+@RefreshScope
 public class BController {
 
-    @NacosValue(value = "${whoami:unkonwn}", autoRefreshed = true)
+    @Value("${whoami:unkonwn}")
     private String whoami;
 
-    @NacosInjected
-    private NamingService namingService;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
-    @PostMapping(path = "/b/try")
+    @PostMapping(path = "/b/tryme")
     public Map<String, String> tryme(@RequestBody Map<String, String> req) throws NacosException {
         Map<String, String> res = new HashMap<>();
         res.put("abc", "def");
         res.put("whoami", whoami);
         System.out.println("who am I : " + whoami);
-        System.out.println(namingService.getAllInstances("hello_b", "helloGroup"));
+
+//        Map<String, String> resFromC = restTemplate.postForObject("http://hello_c/c/confirm", res, Map.class);
+//        System.out.println(resFromC);
+
+//        return resFromC;
         return res;
     }
-
-
-
 }
